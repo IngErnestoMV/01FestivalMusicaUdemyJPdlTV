02d673ee78943a7e8939aec5f180a2c605a39683
IngErnestoMV <ingenieroensistemasernestomv@gmail.com>
2022-08-30 03:50:49 -0500

    Mejorando Código JS con Terser y Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -16,7 +16,7 @@ const webp = require('gulp-webp');
 const avif = require('gulp-avif');
 
 //JavaScript
-const terser = require('terser')
+const terser = require('gulp-terser-js')
 
 function css(done){
     src("src/scss/**/*.scss")    //Identificar el archivo de SASS
@@ -61,6 +61,9 @@ function versionAvif(done){
 
 function javaScript(done){
     src('src/js/**/*.js')
+    .pipe(sourcemaps.init())
+        .pipe(terser())
+        .pipe(sourcemaps.write('.'))
         .pipe(dest('build/js'));
     done();
 }
