4313f425a5b832223a07cfb404ff115146f7f1b1
IngErnestoMV <ingenieroensistemasernestomv@gmail.com>
2022-08-30 02:21:57 -0500

    Fijar la barra al dar scroll

--- build/js/galeria.js
+++ build/js/galeria.js
@@ -11,12 +11,15 @@ function aparecerNavFija(){
     // selecciona elemento clase
     const barra = document.querySelector('.header')
     const sobreFestival = document.querySelector('.sobre-festival')
+    const body = document.querySelector('body');
     //Escucha scroll
     window.addEventListener('scroll', function(){
         if(sobreFestival.getBoundingClientRect().bottom<0){
             barra.classList.add('fijo');
+            body.classList.add('body-scroll');
         } else{
             barra.classList.remove('fijo');
+            body.classList.remove('body-scroll');
         }
     });
 }
